6ac76b973039890760cf85971e3e739b841c4437
Luis Castillo <luiscastillo@iwebsapp.com>
2018-08-14 03:53:10 -0500

    menu-slide

--- server/html/resources/js/main.js
+++ server/html/resources/js/main.js
@@ -4,4 +4,59 @@ $(document).ready(function(){
       location.href = "../../"
     });
 
+    var contProfile = 0
+    $(".submenu").slideUp()
+
+    $(".titleProfile").click( function(e){
+        e.preventDefault()
+        if(contProfile === 0){
+          $(".submenu").slideUp()
+          $(".submenuProfile").slideDown()
+          contProfile = 1
+        } else {
+          $(".submenuProfile").slideUp()
+          contProfile = 0
+        }
+    })
+
+
+    $(".titlePlaces").click( function(e){
+        e.preventDefault()
+        if(contProfile === 0){
+          $(".submenu").slideUp()
+          $(".submenuPlaces").slideDown()
+          contProfile = 1
+        } else {
+          $(".submenuPlaces").slideUp()
+          contProfile = 0
+        }
+    })
+
+
+    $(".titleCoupones").click( function(e){
+        e.preventDefault()
+        if(contProfile === 0){
+          $(".submenu").slideUp()
+          $(".submenuCoupons").slideDown()
+          contProfile = 1
+        } else {
+          $(".submenuCoupons").slideUp()
+          contProfile = 0
+        }
+    })
+
+
+
+    $(".titleShopping").click( function(e){
+        e.preventDefault()
+        if(contProfile === 0){
+          $(".submenu").slideUp()
+          $(".submenuShopping").slideDown()
+          contProfile = 1
+        } else {
+          $(".submenuShopping").slideUp()
+          contProfile = 0
+        }
+    })
+
 });
